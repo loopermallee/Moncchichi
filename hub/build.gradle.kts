aff4c8406ed083a3f4c4903148780303c573e992
loopermallee <whisperingwoods91@gmail.com>
2025-09-28 01:17:08 +0800

    feat: redesigned Device screen with scrollable Material layout

--- hub/build.gradle.kts
+++ hub/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.icons.extended)
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
     implementation(project(":service"))
