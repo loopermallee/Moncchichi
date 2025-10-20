1f692871292414e15ca1ab68adce27fa943f9320
loopermallee <whisperingwoods91@gmail.com>
2025-10-20 10:44:12 +0800

    Add connect/disconnect toggle for hub BLE service

--- hub/src/main/java/com/loopermallee/moncchichi/hub/G1HubApplication.kt
+++ hub/src/main/java/com/loopermallee/moncchichi/hub/G1HubApplication.kt
@@ -1,7 +1,17 @@
 package com.loopermallee.moncchichi.hub
 
 import android.app.Application
+import android.content.Intent
+import androidx.core.content.ContextCompat
+import com.loopermallee.moncchichi.service.G1DisplayService
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class G1HubApplication : Application()
+class G1HubApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val intent = Intent(this, G1DisplayService::class.java)
+        ContextCompat.startForegroundService(this, intent)
+    }
+}
